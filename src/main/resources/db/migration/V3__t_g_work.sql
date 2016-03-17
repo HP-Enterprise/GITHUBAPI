e0ab96c46545be3bb142efcf9e7675f11810715d
chentao <xgct0505@qq.com>
2016-03-17 19:36:37 +0800

    增加sql

---
+++ src/main/resources/db/migration/V3__t_g_work.sql
@@ -0,0 +1,10 @@
+
+create table g_work(
+  id int(11) NOT NULL AUTO_INCREMENT ,
+  name VARCHAR (10) NOT NULL ,
+  finished_work int(6) COMMENT '已完成工作量',
+  unfinished_work int(6) COMMENT '未完成工作量',
+  work_hours int(6) COMMENT '工作时长',
+  week_in_year int(2) COMMENT '本周的序号'
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='issue结果表';
