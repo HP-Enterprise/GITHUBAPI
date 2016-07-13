4d930aaf4310b7d085c63afda285c8a587dc57eb
keqishou <keqishou@qq.com>
2016-07-13 08:51:47 +0800

    添加模糊查询和真实姓名

--- src/main/java/com/incar/gitApi/repository/WorkDetailRepository.java
+++ src/main/java/com/incar/gitApi/repository/WorkDetailRepository.java
@@ -15,9 +15,9 @@ import org.springframework.stereotype.Repository;
 public interface WorkDetailRepository extends CrudRepository<WorkDetail,Integer> {
     @Query("select w from WorkDetail w where w.userName like ?1 and w.week= ?2 and w.year=?3")
     Page<WorkDetail> findPage(String userName,Integer week,Integer year,Pageable pageable);
-   @Query("select w from WorkDetail w where (?1 is null or w.userName = ?1)and" +
-           "(?2 is null or w.project = ?2)and " +
-           "(?3 is null or w.state= ?3) and " +
+   @Query("select w from WorkDetail w where (?1 is null or w.userName like ?1)and" +
+           "(?2 is null or w.project like ?2)and " +
+           "(?3 is null or w.state like ?3) and " +
            "(?4 is null or w.week = ?4) and " +
            "(?5 is null or w.month = ?5)and" +
            "(?6 is null or w.year = ?6)")
